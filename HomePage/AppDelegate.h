8ff868505b0f51149af6189799607ffebc4283cc
葛 彪 <biaoge@hyoukatekiMacBook-Air.local>
2014-07-29 15:43:43 +0800

    Initial Commit

---
+++ HomePage/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HomePage
+//
+//  Created by 葛 彪 on 14-7-29.
+//  Copyright (c) 2014年 BiaoGe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HomePage/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HomePage
+//
+//  Created by 葛 彪 on 14-7-29.
+//  Copyright (c) 2014年 BiaoGe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
